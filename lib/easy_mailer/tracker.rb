18e131ce1b3a2ff3521f27f7f6468269027ae482
Raphaël Joie <raphael.joie@gmail.com>
2017-10-09 20:18:30 +0200

    take Tracker host option from Rails if available

--- lib/easy_mailer/tracker.rb
+++ lib/easy_mailer/tracker.rb
@@ -8,7 +8,15 @@ module EasyMailer
 
     class << self
       def setup
+        if defined?(Rails)
+          default_url_options = Rails.configuration.action_mailer.default_url_options || {}
+
+          EasyMailer::Tracker::Options.host = default_url_options[:host]
+        end
+
         yield(EasyMailer::Tracker::Options) if block_given?
+
+        # TODO raise error if required parameters (i.e. host) is not present
       end
 
       def signature_secret
