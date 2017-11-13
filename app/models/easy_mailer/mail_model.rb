e9506ec3e020e84a047e64462e0e2a2652a55e1a
Raphaël Joie <raphael.joie@webiteasy.be>
2017-11-13 21:08:54 +0100

    Fix : default MailModel not Rails < 5 compatible - use "optional" argument only when Rails.version >= 5

--- app/models/easy_mailer/mail_model.rb
+++ app/models/easy_mailer/mail_model.rb
@@ -2,6 +2,10 @@ module EasyMailer
   class MailModel < ActiveRecord::Base
     self.table_name = "easy_mailer_mails"
 
-    belongs_to :user, polymorphic: true, optional: true
+    if Rails.version >= '5'
+      belongs_to :user, polymorphic: true, optional: true
+    else
+      belongs_to :user, polymorphic: true
+    end
   end
 end
