6c72075a1d7ec2611c39a53331d93576eb098165
Raphaël Joie <raphael.joie@webiteasy.be>
2018-02-12 00:33:40 +0100

    Do not call mail callbacks if mail.perform_deliveries is false

--- lib/easy_mailer/mail/observer.rb
+++ lib/easy_mailer/mail/observer.rb
@@ -2,6 +2,9 @@ module EasyMailer
   module Mail
     class Observer
       def delivered_email(mail)
+
+        return unless mail.perform_deliveries
+
         if EasyMailer.tracker_enabled?
           EasyMailer::Tracker::MailCallback.new(mail).observe
         end
